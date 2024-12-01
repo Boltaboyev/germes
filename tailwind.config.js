56b6c49a36ab8738e79be8636f7ae41b1dc07136
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-01 23:59:04 +0500

    germes

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html}"],
+    theme: {
+        colors: {
+            1: "#5661cb",
+            2: "#272727",
+            red: "#d41a36",
+            org: '#f2994a',
+            br: '#8686864d',
+        },
+        fontFamily: {
+            sans: 'Roboto, sans-serif'
+        },
+        extend: {},
+    },
+    plugins: [],
+}
